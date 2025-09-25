dae0b1cace3b4c3e7bf6000b9da5ca31e5807479
AndrewsSharvinFindmena <42992893+Sharvin05@users.noreply.github.com>
2025-09-25 19:31:30 +0530

    headers sent direct

---
+++ src/Services/ServerApi/server.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+
+
+const serverApi = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3011',
+    withCredentials: true,
+})
+
+serverApi.defaults.timeout = 10000
+
+serverApi.interceptors.request.use(
+    (config) => {
+        config.headers['serverSide'] = 'true'
+        return config
+    },
+    (error) => {
+
+        return Promise.reject(error)
+    },
+
+)
+
+serverApi.interceptors.response.use(
+    (response) => {
+        const code = response.status
+        if (code == 200) {
+            return response.data
+        } else {
+            return Promise.reject(response?.response?.data || 'request error')
+        }
+    },
+    (error) => {
+        return Promise.reject(error?.response?.data)
+    }
+)
+
+export async function getProductsSSR({ params, accessToken, refreshToken }) {
+  return get('products',{accessToken,refreshToken,...params})
+}
+
+export function post(url, data = {}) {
+    return new Promise(function (resolve, reject) {
+        serverApi
+            .post(url, data)
+            .then((res) => {
+                resolve(res)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+
+
+export function get(url, data = {},cookies) {
+    return new Promise(function (resolve, reject) {
+        serverApi
+            .get(url, {
+                params: data,
+                cookies
+            })
+            .then((res) => {
+                resolve(res)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+
